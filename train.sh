47ae5bfb1a72edb3cd4897a4bd1301ed2f5a33c8
Cheng An Hsieh <chenganhsieh1997@gmail.com>
2023-10-23 11:20:56 -0400

    add CLIP code

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
+export CUDA_VISIBLE_DEVICES=4,5
 if [ $# -lt 3 ]
 then
     echo "Usage: bash $0 CONFIG WORK_DIR GPUS"
@@ -15,8 +16,8 @@ PORT=${PORT:-29500}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 
 if [ ${GPUS} == 1 ]; then
-    python $(dirname "$0")/train_ocrclip.py  $CONFIG --work-dir=${WORK_DIR} ${@:4}
+    python $(dirname "$0")/main.py  $CONFIG --work-dir=${WORK_DIR} ${@:4}
 else
     python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-        $(dirname "$0")/train_ocrclip.py $CONFIG --work-dir=${WORK_DIR} --launcher pytorch ${@:4}
+        $(dirname "$0")/main.py $CONFIG --work-dir=${WORK_DIR} --launcher pytorch ${@:4}
 fi
